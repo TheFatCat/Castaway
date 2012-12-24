e2d831385b3b6ba8ce6b377c019b8bca006f2bb0
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2012-12-24 13:29:30 -0800

    Added basics to new player controller
    Still not 100% finished

--- CastawayGame/Assets/scripts/PlayerController2.cs
+++ CastawayGame/Assets/scripts/PlayerController2.cs
@@ -1,25 +1,60 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerScriptTwo : MonoBehaviour {
+public class PlayerController2 : MonoBehaviour {
 	CollisionFlags collisionFlags;
+	CharacterController controller;
+	float currentMaxSpeed;
+	public float maxGroundSpeed = 10;
+	public float maxCrouchSpeed = 7;
+	public float maxAirSpeed = 8;
+	public float jumpSpeed = 5;
+	public float groundAcceleration = 1;
+	public float crouchAcceleration = 1;
+	public float airAcceleration = 1;
+	public float gravityAcceleration = 9.8f;
+	float currentYSpeed = 0;
+	float currentXSpeed = 0 ;
 	// Use this for initialization
 	void Start () {
-	
+		controller = GetComponent<CharacterController>();
 	}
-	float fallSpeed = 0;
+	
 	// Update is called once per frame
 	void Update () {
-		collisionFlags = GetComponent<CharacterController>().Move(new Vector3(Input.GetAxisRaw("Horizontal"),0,0) * 5 *Time.deltaTime);
 		
+		
+		float horizontalInput = Input.GetAxisRaw("Horizontal");
+		float verticalInput = Input.GetAxisRaw("Vertical");
+		
+		//for jumping and falling
+		collisionFlags = controller.Move(new Vector3(0,-1 * currentYSpeed,0)  *Time.deltaTime);
+		Debug.Log("is grounded " + IsGrounded());
+		
+		//in the air
 		if(! IsGrounded()){
-			fallSpeed += 9.8f * Time.deltaTime;
-			collisionFlags = GetComponent<CharacterController>().Move(new Vector3(0,-1 * fallSpeed,0) * 5 *Time.deltaTime);
+			currentXSpeed = Mathf.Lerp(currentXSpeed, horizontalInput * maxAirSpeed, Time.time * airAcceleration);
+			//accelerate downward
+			currentYSpeed += gravityAcceleration * Time.deltaTime;
 				
 		}
+
 		else{
-			fallSpeed = 0;
+			//fall speed should never be exacly 0 or is grounded doesnt work
+			currentYSpeed = 0.1f;
+			currentXSpeed = Mathf.Lerp(currentXSpeed, horizontalInput * maxAirSpeed, Time.time * groundAcceleration );
+
+			if(Input.GetButtonDown("Jump")){
+				Debug.Log("Jumped");
+				currentYSpeed -= jumpSpeed;
+			}
+			
 		}
+		
+		//move from controller input
+		collisionFlags = controller.Move(new Vector3(currentXSpeed,0,0)  *Time.deltaTime);
+		
+
 	
 	}
 	
