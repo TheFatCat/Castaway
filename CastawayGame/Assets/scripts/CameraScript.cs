326f11127850a19802e0b942f6bf0b3e04327a29
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2013-01-03 19:26:55 -0800

    fixed camera orientation

--- CastawayGame/Assets/scripts/CameraScript.cs
+++ CastawayGame/Assets/scripts/CameraScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 	public Transform target;
 	public float cameraDistance = 10f;
+	public float cameraHeight = 10f;
 	public float cameraSpeed = 1f;
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,7 @@ public class CameraScript : MonoBehaviour {
 	void Update () {
 		if(target != null){
 			
-			transform.position = Vector3.Lerp(transform.position , new Vector3(target.position.x , target.position.y + 4 , target.position.z - cameraDistance), cameraSpeed);
+			transform.position = Vector3.Lerp(transform.position , new Vector3(target.position.x , target.position.y + cameraHeight , target.position.z - cameraDistance), cameraSpeed);
 		}
 	}
 }
