248e96412d0248fc9ad63b9195f785d82ebeac42
Sasha Anferov <grangerave@yahoo.com>
2013-01-03 20:37:54 -0800

    added rumbling and boundaries to camera!

--- CastawayGame/Assets/scripts/CameraScript.cs
+++ CastawayGame/Assets/scripts/CameraScript.cs
@@ -5,7 +5,14 @@ public class CameraScript : MonoBehaviour {
 	public Transform target;
 	public float cameraDistance = 29.7299f;
 	public float cameraHeight = 16.1712f;
-	public float cameraSpeed = 1f;
+	public float Ymin = 23.9f;
+	public float Ymax = 100.0f;
+	public float Xmin = -271f;
+	public float Xmax = 117f;
+	public float XminMoveDist = 15f;
+	public float YminMoveDist = 12f;
+	public float rumbleAmount = 0.0f;
+	public float rumbleTimer = 0.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,8 +21,18 @@ public class CameraScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(target != null){
-			
-			transform.position = new Vector3(target.position.x , target.position.y + cameraHeight , target.position.z - cameraDistance); 
+			transform.position = new Vector3(Mathf.Clamp(target.position.x,Xmin,Xmax) ,Mathf.Clamp(target.position.y + cameraHeight,Ymin,Ymax), target.position.z - cameraDistance);
+			//if rumbling
+			if(rumbleTimer > 0.0f){
+				transform.position += new Vector3(Random.Range(-rumbleAmount,rumbleAmount) * Time.deltaTime,Random.Range(-rumbleAmount,rumbleAmount) * Time.deltaTime,0.0f);	//move randomly
+				transform.position = new Vector3(Mathf.Clamp(transform.position.x,Xmin,Xmax) ,Mathf.Clamp(transform.position.y,Ymin,Ymax),transform.position.z);//stay in bounds
+				rumbleTimer -= Time.deltaTime;//subtract timer
+			}
 		}
+	}
+
+	public void Rumble (float amount, float time){
+		rumbleAmount = amount;
+		rumbleTimer = time;
 	}
 }
