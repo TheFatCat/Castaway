4bae2b4819fb305175495a65f91c158bf79f4aa0
Sasha Anferov <grangerave@yahoo.com>
2013-01-03 19:34:45 -0800

    Revert "fixed the camera..?"
    This reverts commit 1b9109d57d69e5218eedd4eff1d44c1b1d9f4762.

--- CastawayGame/Assets/scripts/CameraScript.cs
+++ CastawayGame/Assets/scripts/CameraScript.cs
@@ -3,14 +3,9 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 	public Transform target;
-<<<<<<< HEAD
 	public float cameraDistance = 10f;
 	public float cameraHeight = 10f;
 	public float cameraSpeed = 1f;
-=======
-	public float cameraDistance = 29.729f;
-	public float cameraHeight = 16.17124f;
->>>>>>> fixed the camera..?
 	// Use this for initialization
 	void Start () {
 	
@@ -19,12 +14,8 @@ public class CameraScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(target != null){
-<<<<<<< HEAD
 			
 			transform.position = Vector3.Lerp(transform.position , new Vector3(target.position.x , target.position.y + cameraHeight , target.position.z - cameraDistance), cameraSpeed);
-=======
-			transform.position = new Vector3(target.position.x , target.position.y + cameraHeight , target.position.z - cameraDistance);
->>>>>>> fixed the camera..?
 		}
 	}
 }
