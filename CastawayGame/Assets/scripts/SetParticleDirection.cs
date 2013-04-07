bcfe7a474c7375ea3fddc63f05b9e1594d5bdf4e
Sasha Anferov <grangerave@yahoo.com>
2013-04-07 11:47:17 -0700

    Added rotation control for particles
    A script that sets the particle direction according to parent

---
+++ CastawayGame/Assets/scripts/SetParticleDirection.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class SetParticleDirection : MonoBehaviour {
+
+	public ParticleSystem ps;
+
+	void Start () {
+		ps = this.GetComponent<ParticleSystem>();
+		ps.startRotation = Mathf.Deg2Rad * transform.parent.eulerAngles.z + Random.Range(-0.3f,0.3f);
+		Debug.Log(Mathf.Deg2Rad * transform.parent.eulerAngles.z);
+	}
+	
+
+	void Update () {
+		//nothing to see here
+	}
+}
