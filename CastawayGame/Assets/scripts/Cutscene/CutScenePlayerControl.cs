0facee74c2fd97536eaa6161b4c8e7205c08f5a9
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2013-07-11 18:44:29 -0700

    More work on cutscen

---
+++ CastawayGame/Assets/scripts/Cutscene/CutScenePlayerControl.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class CutScenePlayerControl : CutSceneElement
+{
+	
+	int currentButton;
+	public override void ActionLogic(float deltaTime){
+	
+	}
+}
+
