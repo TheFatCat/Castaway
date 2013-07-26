dc658b6f4eabdf0e7817cbb6423bec0042b8a39c
Sasha Anferov <grangerave@yahoo.com>
2013-07-26 16:51:58 -0700

    added particles to water jumps

--- CastawayGame/Assets/scripts/Player/shadow.cs
+++ CastawayGame/Assets/scripts/Player/shadow.cs
@@ -7,17 +7,32 @@ public class shadow : MonoBehaviour {
 	public Transform parent;
 	public float maxDistance = 10f;
 	public LayerMask mask;
+	public Transform exitParticle;
+	private bool engaged = false;
 
 	// Update is called once per frame
 	void Update ()
 		{
 			//if there is something below the parent, put us there
 			
+
 			RaycastHit hit;
 			if (Physics.Raycast (parent.position + (Vector3.up * 2.0f), -Vector3.up, out hit, maxDistance, mask)) {
-						transform.position = hit.point;
+				transform.position = hit.point;	
+				if (exitParticle != null && engaged == false) {
+					
+					Instantiate (exitParticle, transform.position, Quaternion.identity);
+				}
+
+				engaged = true;
+				
 			} else {//nothing below us
-				transform.position = parent.position - (Vector3.up * 20f);
+						if (exitParticle != null && engaged == true) {
+								
+								Instantiate (exitParticle, transform.position, Quaternion.identity);
+						}
+						engaged = false;
+					transform.position = parent.position - (Vector3.up * 20f);
 			}
 	
 
