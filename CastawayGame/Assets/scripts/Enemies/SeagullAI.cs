709598d9d310326d46985603ab46b4ac7f010941
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2013-08-02 01:26:48 -0700

    Really shitty yet slightly hilarious seagull ai

--- CastawayGame/Assets/scripts/Enemies/SeagullAI.cs
+++ CastawayGame/Assets/scripts/Enemies/SeagullAI.cs
@@ -10,9 +10,13 @@ public class SeagullAI : MonoBehaviour {
 	[SerializeField] Animation dive;
 	EnemyController controller;
 	Animator animator;
+	
+	private bool swooping = false;
+	private float a = 0;
+	private float b = 0;
+	private float t = 0;
 	// Use this for initialization
 	void Start () {
-		
 		controller = GetComponent<EnemyController>();
 		animator = GetComponent<Animator>();
 		controller.setXSpeed(xSpeedScale);
@@ -22,30 +26,48 @@ public class SeagullAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Debug.Log(controller.getXSpeed());
-		timeSinceTurned += Time.deltaTime;
-		
-		controller.setYSpeed( 3 * Mathf.Sin( 3 * Time.time));
-		if(timeSinceTurned > turnWait){
-			timeSinceTurned = 0;
-			turnWait = Random.Range(5,20);
-			if(controller.getXSpeed() > 0){
-				controller.setXSpeed(-1 * xSpeedScale);
-			}
-			else{
-				controller.setXSpeed( xSpeedScale);
-
-			}
-		}
-		
-		
-		if(controller.getXSpeed() > 0 ){
-			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.lossyScale.y, transform.lossyScale.z);
+		if(!swooping){
+			timeSinceTurned += Time.deltaTime;
+			
+			controller.setYSpeed( 3 * Mathf.Sin( 3 * Time.time));
+			if(timeSinceTurned > turnWait){
+				timeSinceTurned = 0;
+				turnWait = Random.Range(5,20);
+				if(controller.getXSpeed() > 0){
+					controller.setXSpeed(-1 * xSpeedScale);
+				}
+				else{
+					controller.setXSpeed( xSpeedScale);
+	
+				}
 				
+			}
+			if(Vector3.Distance(transform.position,PlayerController.getPlayer().position) < 100 ){
+				swooping = true;
+				Vector3 position = PlayerController.getPlayer().position + new Vector3(0,3f,0f);
+				float dX = position.x - transform.position.x;
+				float dY = position.y - transform.position.y;
+				t = 0f;
+				a = dY / 9f;
+				b = dX / 3f;
+			}
 		}
+		else{
+			float deltaTime = Time.deltaTime;
+			t += deltaTime;
+			controller.setXSpeed(b);
+			controller.setYSpeed(2 * -1 * (t - 3) * a);
+			if(t > 6){
+				swooping = false;
+			}
+		}
+		if(controller.getXSpeed() > 0 ){
+				transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.lossyScale.y, transform.lossyScale.z);
+					
+			}
 		else{
 			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.lossyScale.y, transform.lossyScale.z);
 		}
-		
 		/*
 		if(ySpeed > 0 ){
 			//animator.setAnimation(soar, true);
