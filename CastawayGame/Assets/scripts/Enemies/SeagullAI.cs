17aa11fc69ea747eca9ca203fd5d6068b0c1b775
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2013-08-09 21:29:35 -0700

    fixed seagull swoop

--- CastawayGame/Assets/scripts/Enemies/SeagullAI.cs
+++ CastawayGame/Assets/scripts/Enemies/SeagullAI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 [RequireComponent (typeof(EnemyController))]
 [RequireComponent (typeof(Animator))]
 public class SeagullAI : MonoBehaviour {
+	[SerializeField] float swoopDuration = 3;
 	[SerializeField] float xSpeedScale = 10;
 	[SerializeField] float ySpeedScale = 5;
 	[SerializeField] float swoopDistance = 100;
@@ -13,8 +14,9 @@ public class SeagullAI : MonoBehaviour {
 	Animator animator;
 	
 	private bool swooping = false;
-	private float a = 0;
-	private float b = 0;
+	private float yZero = 0;
+	private int xMultiplier = 1;
+	private float xZero = 0;
 	private float t = 0;
 	// Use this for initialization
 	void Start () {
@@ -49,17 +51,18 @@ public class SeagullAI : MonoBehaviour {
 				Vector3 position = player.position + new Vector3(0f,player.lossyScale.y * 5f,0f);
 				float dX = position.x - transform.position.x;
 				float dY = position.y - transform.position.y;
-				t = 0f;
-				a = dY / 9f;
-				b = dX / 3f;
+				yZero = 4 * dY / swoopDuration; 
+				xZero = dX / swoopDuration;
+				xMultiplier = dX > 1 ? 1:-1;
 			}
 		}
 		else{
 			float deltaTime = Time.deltaTime;
 			t += deltaTime;
-			controller.setXSpeed(b);
-			controller.setYSpeed(2 * -1 * (t - 3) * a);
-			if(t > 6){
+			controller.setXSpeed(xMultiplier*Mathf.Abs(xZero - (xZero / (0.5f * swoopDuration)) * (t - 0.5f * swoopDuration)));
+			controller.setYSpeed(-1 * (t - 0.5f * swoopDuration) *(yZero / (0.5f * swoopDuration)));
+			if(t > swoopDuration){
+				t = 0;
 				swooping = false;
 			}
 		}
