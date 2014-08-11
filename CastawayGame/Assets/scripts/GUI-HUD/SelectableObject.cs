2b78fe386f040649396ae96a996338c3ed477deb
Sasha Anferov <grangerave@yahoo.com>
2014-08-10 20:41:52 -0700

    Added dummy buttons and animations

---
+++ CastawayGame/Assets/scripts/GUI-HUD/SelectableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectableObject : MonoBehaviour {
+
+	private bool selected = false;
+	private TextMesh tm;
+
+	private Color startcolor;
+	void OnMouseEnter()
+	{	
+		selected = true;
+		tm = GetComponent<TextMesh>();
+		startcolor = tm.color;
+		tm.color = Color.white;
+	}
+	void OnMouseExit()
+	{
+		tm.color = startcolor;
+				selected = false;
+	}
+
+}
