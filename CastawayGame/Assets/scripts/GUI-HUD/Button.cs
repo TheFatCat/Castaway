4ca37f3278c1c317eb4133aee877b2998d9f0c9e
Sasha Anferov <grangerave@yahoo.com>
2014-08-15 19:31:39 -0700

    made real buttons!

---
+++ CastawayGame/Assets/scripts/GUI-HUD/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+	
+	public enum ButtonType {Start,Credits,Quit};
+	
+	public ButtonType myType;
+	
+	void OnMouseDown() {
+			switch(myType){
+				case(ButtonType.Start):
+					Application.LoadLevel("FinalScene");
+					break;
+				case(ButtonType.Credits):
+					Debug.Log("credits...");
+					break;
+				case(ButtonType.Quit):
+					Application.Quit();
+					break;
+			
+			}
+		
+	}
+}
